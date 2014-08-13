f2ab3a7a26e3f367d297e608d738a3ea5285c2f1
Daniel Tomlinson <dan@tomlinson.io>
2014-08-13 22:48:33 +0100

    Fibonacci sequence

---
+++ even-fibonacci-sum/even-fibonacci-sum.rs
@@ -0,0 +1,36 @@
+use std::iter::AdditiveIterator;
+
+use std::mem;
+use std::num::One;
+
+pub fn fibonacci<T: One>() -> Fibonacci<T> {
+    fibonacci_with_init(One::one(), One::one())
+}
+
+pub fn fibonacci_with_init<T>(a0: T, a1: T) -> Fibonacci<T> {
+    Fibonacci { current: a0, next: a1 }
+}
+
+pub struct Fibonacci<T> { current: T, next: T }
+
+impl<T: Add<T,T>> Iterator<T> for Fibonacci<T> {
+    #[inline]
+    fn next(&mut self) -> Option<T> {
+        let new_next    = self.current + self.next;
+        let new_current = mem::replace(&mut self.next, new_next);
+        let retval = mem::replace(&mut self.current,   new_current);
+        Some(retval)
+    }
+}
+
+fn main() {
+	let max = 4000000i;
+	println!("Sequence result is: {}", sequencer(max));
+}
+
+fn sequencer(max: int) -> int {
+	Fibonacci {current: 1i, next: 1i}
+		.take_while(|&v| v < max)
+		.filter(|&v| v % 2 == 0)
+		.sum()
+}
